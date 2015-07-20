86032425d8c43e8dbfabbd58eb9d9615cf0d263c
Dener Carvalho <furiano.k@gmail.com>
2015-07-19 22:12:11 -0300

    Commit BLLProduto
    Finalização classe BLLProduto

--- BBL/BLLProduto.cs
+++ BBL/BLLProduto.cs
@@ -66,22 +66,73 @@ namespace BLL
 
         public void Alterar(ModeloProduto p) 
         {
+            if (p.CodigoProduto <= 0) 
+            {
+                throw new Exception("Selecione um produto para alterar.");
+            }
 
+            if (p.DescricaoProduto.Trim().Length == 0)
+            {
+                throw new Exception("A descrição do Produto é obrigatoria.");
+            }
+
+            if (p.NomeProduto.Trim().Length == 0)
+            {
+                throw new Exception("O nome do produto é obrigatório.");
+            }
+
+            if (p.ValorVenda <= 0)
+            {
+                throw new Exception("O valor da venda deve ser maior que zero.");
+            }
+
+            if (p.Quantidade <= 0)
+            {
+                throw new Exception("A quantidade deve ser maior que zero.");
+            }
+
+            if (p.Quantidade > 1000)
+            {
+                throw new Exception("Valor maximo da quantidade alcaçado para esta operação, o valor deve ser menor ou igual a mil.");
+            }
+
+            if (p.UnidadeMedida <= 0)
+            {
+                throw new Exception("Unidade de medida deve ser informada.");
+            }
+
+            if (p.Categoria <= 0)
+            {
+                throw new Exception("A Categoria deve ser informada.");
+            }
+
+            if (p.SubCategoria <= 0)
+            {
+                throw new Exception("A SubCategoria deve ser informada.");
+            }
+
+            DALProduto da = new DALProduto(CONEXAO);
+            da.Alterar(p);
         }
 
         public void Excluir(int codigo) 
-        { 
-
+        {
+            DALProduto da = new DALProduto(CONEXAO);
+            da.Excluir(codigo);
         }
 
         public DataTable Localizar(string s) 
         {
-            return null;
+            DALProduto da = new DALProduto(CONEXAO);
+
+            return da.Localizar(s);
         }
 
         public ModeloProduto carregaProduto(int codigo) 
         {
-            return null;
+            DALProduto da = new DALProduto(CONEXAO);
+
+            return da.carregaProduto(codigo);
         }
     }
 }
